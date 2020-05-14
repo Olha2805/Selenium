ddba407a8e3a2e5f7f30494de2c10ff571d9cc0d
Olha <qwerty@mail.ru>
2020-05-14 22:01:43 +0300

    HW Selenium Done

--- src/Homework/Selenium1.java
+++ src/Homework/Selenium1.java
@@ -7,18 +7,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Selenium1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://rozetka.com.ua/");
 
-        By search = By.cssSelector("[name='search']");
-        WebElement searchEl = driver.findElement(search);
         Thread.sleep (5000);
-        searchEl.sendKeys("iPhone");
-        Thread.sleep (5000);
-        String name = driver.findElement(By.cssSelector("[class='header-topline__user-libk link-dashed']")).getText();
-        System.out.println(name);
+        String phoneNumber = driver.findElement(By.cssSelector("[class=link-dashed]")).getText();
+        String newphoneNumber = phoneNumber.replace(" ", "").
+                replace(")", "").
+                replace("(", "").
+                replace("-", "");
+        System.out.println("phone number for order is: " + newphoneNumber);
         driver.quit();
 
     }
